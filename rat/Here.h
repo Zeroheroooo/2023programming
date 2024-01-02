2bc017f36fe78e4ffc3bfdc9c780c4894c791768
Zeroheroooo <411221213@gms.ndhu.edu.tw>
2024-01-02 22:46:06 +0800

    rat in maze have .h

--- rat/Here.h
+++ rat/Here.h
@@ -2,21 +2,40 @@
 #define _HERE_H_
 
 #define MAX_N 10
-#define EQUAL(p,q) (p.x == q.x && p.y == q.y)
+#define inside(x,y) ((x) < MAX_N && (y) < MAX_N)
 
-typedef struct coordinate{
+
+typedef struct point{
     int x;
     int y;
 }XY_;
 
 typedef struct stack{
-    XY_ path[MAX_N*MAX_N];
+    XY_ elements[MAX_N*MAX_N];
     int top;
-}pocket_;
+}stack_;
 
 
-void push(pocket_ *stackptr, XY_ xy);
-XY_ pop(pocket_ *stackptr);
-int is_empty(pocket_ *stackptr);
+int is_empty(stack_ *stackptr){
+    if(stackptr->top == -1) return 1;
+    return 0;
+}
+
+int is_full(stack_ *stackptr){
+    if(stackptr->top == MAX_N * MAX_N -1) return 1;
+    return 0;
+}
+
+void push(stack_ *stackptr, XY_ newpoint){
+    if(!is_full(stackptr)){
+        stackptr->top++;
+        stackptr->elements[stackptr->top] = newpoint;
+    }
+}
+
+XY_ pop(stack_ *stackptr){
+    //XY_ tmp={-1,-1};
+    if(!is_empty(stackptr)) return stackptr->elements[stackptr->top--];
+}
 
 #endif
--- rat/rat.c
+++ rat/rat.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_N 10
-#define inside(x,y) ((x) < MAX_N && (y) < MAX_N)
-
-typedef struct point{
-    int x;
-    int y;
-}XY_;
-
-typedef struct stack{
-    XY_ elements[MAX_N*MAX_N];
-    int top;
-}stack_;
+#include "Here.h"
 
 int maze[MAX_N][MAX_N] ={{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
                         { 0, 0, 0, 1, 1, 0, 1, 1, 0, 1 },
@@ -21,35 +10,14 @@ int maze[MAX_N][MAX_N] ={{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
                         { 1, 0, 1, 1, 1, 0, 0, 1, 0, 1 },
                         { 1, 0, 0, 0, 0, 0, 1, 1, 0, 0 },
                         { 1, 0, 1, 1, 1, 1, 1, 0, 0, 1 },
-                        { 1, 0, 0, 0, 1, 0, 0, 0, 1, 1 },
+                        { 1, 0, 0, 0, 0, 0, 0, 0, 1, 1 },
                         { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
                         };
 int mark[MAX_N][MAX_N];
 
-int is_empty(stack_ *stackptr){
-    if(stackptr->top == -1) return 1;
-    return 0;
-}
-
-int is_full(stack_ *stackptr){
-    if(stackptr->top == MAX_N * MAX_N -1) return 1;
-    return 0;
-}
-
-void push(stack_ *stackptr, XY_ newpoint){
-    if(!is_full(stackptr)){
-        stackptr->top++;
-        stackptr->elements[stackptr->top] = newpoint;
-    }
-}
-
-XY_ pop(stack_ *stackptr){
-    //XY_ tmp={-1,-1};
-    if(!is_empty(stackptr)) return stackptr->elements[stackptr->top--];
-}
-
 int main(){
 
+    //copy maze to mark
     for(int i=0;i < MAX_N;i++){
         for(int j=0;j < MAX_N;j++){
             mark[i][j] = maze[i][j];
@@ -59,13 +27,16 @@ int main(){
     stack_ pocket;
     pocket.top = -1;
 
+    //set entrance & exit
     XY_ exit={1,0},origin={6,9},newdir={-1,-1};
+    printf("entrance:(%d,%d)  exit:(%d,%d)\n",origin.x,origin.y,exit.x,exit.y);
 
-    
+    //put in first (x,y)
     push(&pocket,origin);
     mark[origin.x][origin.y] = 1;
     int x,y;
 
+    //////////////////////////////////   use stack    /////////////////////////////////////
     while(mark[exit.x][exit.y] != 1){
         origin = pop(&pocket);
 
@@ -127,7 +98,9 @@ int main(){
         }
 
     }
+    //////////////////////////////////////////////////////////////////////////
 
+    //output route
     stack_ route;
     route.top = -1;
 
